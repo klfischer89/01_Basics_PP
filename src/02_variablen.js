72fa209dba018789f5cfaed825fcbcd09a67ded3
klfischer89 <fischer-k4@web.de>
2025-11-06 11:04:04 +0000

    let vs const

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -8,7 +8,8 @@
  
 /* Deklaration + Wertzuweisung I */
 
-/**let firstName; // Deklaration (Definition)
+/**
+ * let firstName; // Deklaration (Definition)
 console.log(firstName); // Ausgabe
 firstName = "Max"; // Wertzuweisung | Assignment
 console.log("Inhalt: " + firstName); // Ausgabe mit Inhalt
@@ -22,10 +23,11 @@ console.log("Sei gegrüßt" , firstName , familyName + ", lebe lang und in Fried
     JS ist ein untypisierte Sprache!
 */
 
-let test;
+//let test;
 
 /* undefined */
-console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+/**
+ * console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
 console.log("Inhalt: " + test); // Ausgabe Inhalt
 console.log("----------------")
 
@@ -43,3 +45,17 @@ test = true; // boolean
 console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
 console.log("Inhalt: " + test); // Ausgabe Inhalt
 console.log("----------------")
+*/
+
+/* Variablen vs. Konstanten in JS */
+
+// Variable
+let test; // Variable : Deklaration
+test = "hi"; // Wertzuweisung (Initialisierung)
+test = "hello"; // Überschreiben
+console.log("Inhalt: " + test); // Ausgabe 
+
+// Konstante
+const test2 = "Hi!"; // Deklaration + Wertzuweisung gemeinsam
+// test2 = "hello"; // Laufzeit-Fehler!
+console.log("Inhalt",test2); //Ausgabe
