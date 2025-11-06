169168200b678f99ea1a60323a3bef16d4044751
klfischer89 <fischer-k4@web.de>
2025-11-06 10:10:05 +0000

    Deklaration + Wertzuweisung I

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,6 +1,17 @@
 // Zeilenkommentar
-/**
- * 
+
+/** 
  * Block Kommentar
- * 
  */
+
+/***** Variablen 01 *******/
+ 
+/* Deklaration + Wertzuweisung I */
+
+let firstName; // Deklaration (Definition)
+console.log(firstName); // Ausgabe
+firstName = "Max"; // Wertzuweisung | Assignment
+console.log("Inhalt: " + firstName); // Ausgabe mit Inhalt
+let familyName = "Mütze"; // Deklaration & Wertzuweisung
+// Trennen von Strings mit , (entählt Leerzeichen) oder + (kein Leerzeichen)
+console.log("Sei gegrüßt" , firstName + " " + familyName + ", lebe lang und in Frieden!");
