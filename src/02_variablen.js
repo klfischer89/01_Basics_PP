186699129a3f49167c48cc35eac50ce9264be597
klfischer89 <fischer-k4@web.de>
2025-11-06 10:49:45 +0000

    testing different datatypes with one variable

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -8,10 +8,38 @@
  
 /* Deklaration + Wertzuweisung I */
 
-let firstName; // Deklaration (Definition)
+/**let firstName; // Deklaration (Definition)
 console.log(firstName); // Ausgabe
 firstName = "Max"; // Wertzuweisung | Assignment
 console.log("Inhalt: " + firstName); // Ausgabe mit Inhalt
 let familyName = "Mütze"; // Deklaration & Wertzuweisung
 // Trennen von Strings mit , (entählt Leerzeichen) oder + (kein Leerzeichen)
 console.log("Sei gegrüßt" , firstName , familyName + ", lebe lang und in Frieden!");
+*/
+
+/*  
+    Wertzuweisung II + Datentypen 
+    JS ist ein untypisierte Sprache!
+*/
+
+let test;
+
+/* undefined */
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------")
+
+test = "hi"; // string
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------")
+
+test = 2; // number
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------")
+
+test = true; // boolean
+console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+console.log("Inhalt: " + test); // Ausgabe Inhalt
+console.log("----------------")
