378c08e8873b010086e0b99a0e5bd9defb809158
klfischer89 <fischer-k4@web.de>
2025-11-07 10:44:37 +0000

    added file for calculating the age of persons only considering the year

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -3,15 +3,20 @@
 
 // Deklaration
 let ageJohn, ageMark;
-const birthYearJohn = 1980;
+const birthYearJohn = 2000;
 const birthYearMark = 1990;
 
-// Berechnung Alter
-let year = 2025;
+// Berechnung Alter Iteration I
+// let year = 2025;
+// ageJohn = year - birthYearJohn;
+// ageMark = year - birthYearMark;
+
+// Berechnung Alter Iteration II
+let date = new Date();
+let year = date.getFullYear();
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
 
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
-
