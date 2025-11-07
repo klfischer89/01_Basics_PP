fcbcf73486a58ce8510de5df3873cce1298b23b7
klfischer89 <fischer-k4@web.de>
2025-11-07 11:29:42 +0000

    Iteration IV for campare age of two persons, requiring input of names and birth years. Added file for exercising controlflow

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -2,26 +2,39 @@
 /* Berechnung Alter */
 
 // Deklaration
-let ageJohn, ageMark;
-const birthYearJohn = 2000;
-const birthYearMark = 1990;
+const prompt = require('prompt-sync')({sigint: true});
+let agePerson1, agePerson2;
 
-// Berechnung Alter Iteration I
-// let year = 2025;
-// ageJohn = year - birthYearJohn;
-// ageMark = year - birthYearMark;
-
-// Berechnung Alter Iteration II
-// let date = new Date();
-// let year = date.getFullYear();
-// ageJohn = year - birthYearJohn;
-// ageMark = year - birthYearMark;
+const namePerson1 = prompt("Name Person 1: ");
+const namePerson2 = prompt("Name Person 2: ");
+const birthYearJohn = Number(prompt("Geburtsjahr John: "));
+const birthYearMark = Number(prompt("Geburtsjahr Mark: "));
 
 // Berechnung Alter Iteration III
 let year = new Date().getFullYear();
-ageJohn = year - birthYearJohn;
-ageMark = year - birthYearMark;
+agePerson1 = year - birthYearJohn;
+agePerson2 = year - birthYearMark;
 
 // Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
+console.log("agePerson 1: " + agePerson1);
+console.log("agePerson 2: " + agePerson2);
+
+// Deklaration
+let isJohnOlder;
+
+// function for comparison who is older
+function compareAge(){
+    // compare who is older
+    if (agePerson1 > agePerson2) {
+        isJohnOlder = true;
+        console.log(namePerson1, "ist älter.")
+    } else if (agePerson1 < agePerson2){
+        isJohnOlder = false;
+        console.log(namePerson2, "ist älter.")
+    } else {
+        console.log("Beide gleich alt.")
+    }
+}
+
+// call function to determine who is older
+compareAge();
