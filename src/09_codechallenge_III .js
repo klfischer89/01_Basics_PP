2efb55e84bf0f5fe50ff69abcffb9e8e789d4eb1
klfischer89 <fischer-k4@web.de>
2025-11-11 11:34:19 +0000

    started code challenge, added some functions

--- src/09_codechallenge_III .js
+++ src/09_codechallenge_III .js
@@ -8,4 +8,26 @@
 // bestellung.json  - Datei bitte in den online-Editor STRUCTOG laden
 // https://ddi.education/struktog/
 
+// provide functions of prompts in const prompt
+const prompt = require('prompt-sync')({sigint: true});
 
+function welcomeToMD(params) {
+    
+}
+
+function orderFood(params) {
+    
+}
+
+function orderDrinks(params) {
+    
+}
+
+function thankYouAndBye(params) {
+    
+}
+
+// function for outputting a string
+function output(outputData) {
+    console.log(outputData);
+}
