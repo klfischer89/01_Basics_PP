5bcbff6bd10d43deddd2b5b8f393bd3452470f5c
klfischer89 <fischer-k4@web.de>
2025-11-13 14:11:24 +0000

    first iteration for adding key value pair to map by user input

--- src/12_codechallenge_IV.js
+++ src/12_codechallenge_IV.js
@@ -10,51 +10,69 @@
 
 // Parameter --> funktion getSentence([ ...], "Q"){ ...}
 
-const ERROR_STR_OP = "Bitte definieren Sie Ihre Sätze nur nach den vereinbarten Arten\nDiese sind:\nS -> .\nQ -> ?\nE -> !"
+// constant strings for errors and infos
+const ERROR_STR_OP = "Bitte definieren Sie Ihre Sätze nur nach den vereinbarten Arten:\nS -> .\nQ -> ?\nE -> !"
 
-let regexOp = /^[SQE]$/;
-let arr, op, arrOp, arrPunct, mapOpPunct;
-
+// provide functions of prompts in const prompt
+const prompt = require('prompt-sync')({sigint: true});
 
 /*** Funktion mit Array als Parameter */
 // mögliche Tests:
-//output(getSentence(["Ich","bin","Max"],"S"));
-//output(getSentence(["Bist","du","Max"],"Q"));
-//output(getSentence(["Ich","bin"],"E"));
-//output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
-
+/**
+output(getSentence(["Ich","bin","Max"],"S"));
+output(getSentence(["Bist","du","Max"],"Q"));
+output(getSentence(["Ich","bin"],"E"));
+output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
+output(getSentence(["Ich","bin","Max"],"a"));
+output(getSentence(["Bist","du","Max"],"C"));
+output(getSentence(["Ich","bin"],1));
+output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],true));
+*/
 function getSentence(arr,op) {
-   // ???
+
+    // initialize map for punctuation marks
+    let map = initOpAndPunct();
+
+    // provide space for strings
+    const GAP = " ";
+
+    // check if given operaotr is a key in the map, if not return error string
+    // if it is return joined array followed by mapped punctuation mark
+    if (!determinePunct(map,op)) {
+        return ERROR_STR_OP
+    } else {
+        return arr.join(GAP) + determinePunct(map,op);
+    }
+    
 }
 
-// Check if there ist a valid operator
-/**
-output(checkOperator("S"));
-output(checkOperator("E"));
-output(checkOperator("Q"));
-output(checkOperator("a"));
-output(checkOperator("C"));
-output(checkOperator("SEF"));
+//output(addPunct());
+function addPunct(map){
+
+    map = initOpAndPunct();
+
+    map.set(prompt("Bitte geben Sie einen neuen Operator ein, der ein Satzzeichen darstellen soll: "),
+            prompt("Bitte geben Sie ein Satzzeichen ein, das durch diesen Operator erzeugt werden soll: "));
+
+    return map;        
+}
+
+// determine punctuation by checking if the operator is included as a key in the map
+/** 
+ output(determinePunct(initOpAndPunct(), "a"))
+ output(determinePunct(initOpAndPunct(), "S"))
+ output(determinePunct(initOpAndPunct(), 1))
 */
-function checkOperator(op) {
-    if (!regexOp.test(op)) {
-        return ERROR_STR_OP;
-    } else {
-        return op.toLowerCase();
+function determinePunct(map, op){
+    if (map.has(op)) {
+        return map.get(op);
     }
 }
 
 // initialize map containing valid operators as keys and punctuations as values 
-output(initOpAndPunct());
+ output(initOpAndPunct());
 function initOpAndPunct(){
-
-    /**
-    arrOp = ["S", "Q", "E"];
-    arrPunct = [".", "?", "!"]
-    mapOpPunct = new Map(arrOp.map((key, i) => [key, arrPunct[i]]));
-    */
-   
-    map = new Map([
+    let map = new Map([
         ["S", "."],
         ["Q", "?"],
         ["E", "!"],
@@ -62,7 +80,6 @@ function initOpAndPunct(){
 
     return map;
 }
-
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
