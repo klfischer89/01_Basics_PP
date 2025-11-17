687acb9faefec41eae19c796720605e35faf919d
klfischer89 <fischer-k4@web.de>
2025-11-17 08:57:31 +0000

    added file for learning nested arrays, loops, and data structures for objects. Modified package.json to run this file

---
+++ src/14_objects.js
@@ -0,0 +1,50 @@
+/***** Erinnerung: Arrays *******/
+
+// let arr;
+// arr = ["Ich","bin","Max"];
+// output(arr);
+// output(arr[0]);
+// output("------------");
+
+// nested arrays - Prinzip: [[[]]]
+// nested arrays  - hier: [[],[]]
+/**arr =   [
+            ["Ich","bin","Max"],
+            ["Ich","bin","Moritz"]
+        ];
+      
+// output(arr);
+// output(arr[0]);
+// output(arr[0][0]+ " " + arr[1][2]);
+// output(arr[1][0]);
+// output("------------");
+
+// // nested loop
+for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+         output("Index i: " + i + " Index j: " + j);
+         output(arr[i][j]);       
+    }
+}
+*/
+
+/***** Objekte 1 Daten/Funktionen *******/
+
+let person = {
+    firstName:"Felix",
+    familyName:"Wolf",
+    salary:[120000, 160000],
+    permission:true
+};
+
+output(person);
+output(person.firstName);
+output(person.familyName);
+output(person.salary[1]);
+output(person.permission);
+
+// Modul: Ausgabe | Test
+//output("hi");
+function output(outputData) {
+    console.log(outputData);
+}
